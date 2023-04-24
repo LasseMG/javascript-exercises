c5b6bc6c118372d0cc823534fa04641f54a43ced
Lasse <lasse.gelsbjerg@gmail.com>
2023-04-24 20:57:44 +0200

    complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-    let result = 0
-    for (i = 1; i <= 4000; i++) {
+const sumAll = function(start, end) {
+    let result = 0;
+
+    if (start < 0 || end < 0) {
+        return "ERROR";
+        //negative values return an error
+    } else if (!Number.isFinite(start) || !Number.isFinite(end)) {
+        return "ERROR";
+        //check if values are strictly numbers 
+    } else if (start > end) {
+        let temp = start; 
+        start = end; 
+        end = temp;
+        //temporary variable to allow bigger number first 
+    }
+
+    for (i = start; i <= end; i++) {
         result += i;
     }
     return result; 
