47bcdc31ebf693c03dc7947f13af79e264d578f4
Lasse <lasse.gelsbjerg@gmail.com>
2023-04-25 21:46:37 +0200

    add comment to assignment 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,6 +2,7 @@ const leapYears = function(anyYear, i) {
     
     for (i = 0; i <= anyYear; i++) {
         if (anyYear % 4 === 0 && (anyYear % 100 !== 0 || anyYear % 400 === 0)) {
+            //remainder must be 0, when divided by 100 remainder must not be 0 OR remainder must be 0 when divided by 400 
             return true; 
         } else {
             return false;
