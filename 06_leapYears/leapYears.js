9ffe1c40eb902293150842f3bff8219ab548ab60
Lasse <lasse.gelsbjerg@gmail.com>
2023-04-25 21:20:28 +0200

    complete assignment 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(anyYear, i) {
+    
+    for (i = 0; i <= anyYear; i++) {
+        if (anyYear % 4 === 0 && (anyYear % 100 !== 0 || anyYear % 400 === 0)) {
+            return true; 
+        } else {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
